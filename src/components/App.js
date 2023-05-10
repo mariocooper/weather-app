8edece272ac3026f64634c06c41bb18557facdc2
Mario Cooper <mariocooper00@hotmail.com>
2023-05-10 13:20:20 +0100

    styling added to the weather app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import "../styles/App.css";
 import React from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+
+import "../styles/App.css";
 
 function App({ location, forecasts }) {
   const { city, country } = location;
 
   return (
-    <div className="app">
+    <div className="weather-app">
       <LocationDetails city={city} country={country} />
       <ForecastSummaries forecasts={forecasts} />
     </div>
