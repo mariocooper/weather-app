d1f4e0d48ea4686795f581d9ff1ee3e0c223a0ea
Mario Cooper <mariocooper00@hotmail.com>
2023-05-10 12:49:12 +0100

    forecast summmaries component added, tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import "../styles/App.css";
 import React from "react";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
+function App({ location, forecasts }) {
   const { city, country } = location;
 
   return (
     <div className="app">
       <LocationDetails city={city} country={country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -3,13 +3,14 @@ import React from "react";
 function ForecastSummary(props) {
   const { date, description, icon, temperature } = props;
   return (
-    <div className="forecast-summary">
+    <div data-testid="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__description">{description}</div>
       <div className="forecast-summary__icon">{icon}</div>
       <div className="forecast-summary__temperature">
         {temperature.max}&deg;C
       </div>
+      <div className="forecast-summary__description">{description}</div>
     </div>
   );
 }
