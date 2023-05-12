e53d35063c81064e6d0310376cd025ab2fce3db9
Mario Cooper <mariocooper00@hotmail.com>
2023-05-13 00:40:56 +0100

    dates and icons formatted, all tests passing & snapshots updated

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,16 +1,21 @@
 import React from "react";
+import iconData from "../data/iconData.json";
 
 function ForecastSummary(props) {
   const { date, description, icon, temperature } = props;
+  const weatherCode = `${icon.slice(0, 1)}00`;
+  const formattedDate = new Date(date).toDateString();
+
   return (
-    <div data-testid="forecast-summary">
-      <div className="forecast-summary__date">{date}</div>
+    <div className="forecast-summary" data-testid="forecast-summary">
+      <div className="forecast-summary__date">{formattedDate}</div>
       <div className="forecast-summary__description">{description}</div>
-      <div className="forecast-summary__icon">{icon}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        <img src={iconData[weatherCode]} alt="Forecast Icon" />
+      </div>
       <div className="forecast-summary__temperature">
         {temperature.max}&deg;C
       </div>
-      <div className="forecast-summary__description">{description}</div>
     </div>
   );
 }
