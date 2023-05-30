424a3824d84a1078908c17003f90cb16403257b9
Mario Cooper <mariocooper00@hotmail.com>
2023-05-30 20:32:17 +0100

    css styling&background photo changed-app complete

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -9,13 +9,14 @@ function ForecastSummary(props) {
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{formattedDate}</div>
-      <div className="forecast-summary__description">{description}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
         <img src={iconData[weatherCode]} alt="Forecast Icon" />
       </div>
+      <div className="forecast-summary__description">{description}</div>
       <div className="forecast-summary__temperature">
         {temperature.max}&deg;C
       </div>
+      <br />
       <button type="button" onClick={() => onSelect(date)}>
         More Details
       </button>
